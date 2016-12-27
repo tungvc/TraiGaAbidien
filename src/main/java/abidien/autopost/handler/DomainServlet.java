dfc240db1364760668a80bd2b4f4a90aeb59098d
Võ Cao Tùng <tungvc2@vng.com.vn>
2016-12-27 14:17:52 +0700

    no message

--- src/main/java/abidien/autopost/handler/DomainServlet.java
+++ src/main/java/abidien/autopost/handler/DomainServlet.java
@@ -38,13 +38,13 @@ public class DomainServlet extends RestServlet<DomainEntity> {
 
     @Invoke(params = "request,response")
     public void domain(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        /*int userId = Helper.getUser(request).getId();
+        int userId = Helper.getUser(request).getId();
         List<DomainEntity> data = Environment.getDomainService().loadAll().stream()
                 .filter(p -> p.getOwnerId() == userId)
-                .collect(Collectors.toList());*/
-        List<DomainEntity> data = Environment.getDomainService().loadAll();
+                .collect(Collectors.toList());
+        //List<DomainEntity> data = Environment.getDomainService().loadAll();
         if (data != null && data.size() > 0)
-            data.sort((x1, x2) -> x2.getId()- x1.getId());
+            data.sort((x1, x2) -> (x2.getId() == null ? 0 : x2.getId()) - (x1.getId() == null ? 0 : x1.getId()));
         request.setAttribute("domainList", data);
         Helper.forwardAutoPostPage(this, request, response, "APDomain");
     }
